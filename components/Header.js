efa6c51994fff0e4a3e7de2575893626ed9e996b
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-04-28 17:06:28 +0200

    Add searchbar

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
+import { SearchIcon } from '@heroicons/react/outline';
 
 const Header = () => {
   return (
@@ -17,6 +18,21 @@ const Header = () => {
           layout="fill"
           className="object-contain"
         />
+      </div>
+
+      <div className="relative mt-1">
+        <div className="absolute top-2 left-2">
+          <SearchIcon className="h-5 text-gray-500" />
+        </div>
+        <input
+          type="search"
+          placeholder="Search"
+          className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
+        />
+      </div>
+
+      <div>
+        <h2>Right Side</h2>
       </div>
     </div>
   );
