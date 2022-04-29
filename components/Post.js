551175fdcdcce98a80df8e352755de8fb1169646
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-04-29 18:55:37 +0200

    Add posts header and image

--- components/Post.js
+++ components/Post.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { DotsHorizontalIcon } from '@heroicons/react/outline';
 
 const Post = ({ id, username, userImg, img, caption }) => {
   return (
-    <div>
-      <h2>{username}</h2>
+    <div className="bg-white rounded-md border my-7">
+      <div className="flex items-center p-5">
+        <img
+          className="h-12 object-cover rounded-full border p-1 mr-3"
+          src={userImg}
+          alt={username}
+        />
+        <p className="font-bold flex-1">{username}</p>
+        <DotsHorizontalIcon className="h-5" />
+      </div>
+
+      <img className="object-cover w-full" src={img} alt="Post image" />
     </div>
   );
 };
