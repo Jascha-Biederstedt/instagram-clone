34c4ed028fbaec8c53218f2916d320fc9691fd2e
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-05-03 15:18:06 +0200

    Add user story with google data

--- components/Stories.js
+++ components/Stories.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import minifaker from 'minifaker';
 import 'minifaker/locales/en';
+import { useSession } from 'next-auth/react';
 
 import Story from './Story';
 
 const Stories = () => {
   const [storyUsers, setStoryUsers] = useState([]);
+
+  const { data: session } = useSession();
 
   useEffect(() => {
     const generatedStoryUsers = minifaker.array(20, i => ({
@@ -19,6 +22,14 @@ const Stories = () => {
 
   return (
     <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border overflow-x-scroll rounded-sm scrollbar-none">
+      {session && (
+        <Story
+          username={session.user.username}
+          img={session.user.image}
+          isUser
+        />
+      )}
+
       {storyUsers.map(user => (
         <Story key={user.id} username={user.username} img={user.img} />
       ))}
--- components/Story.js
+++ components/Story.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { PlusIcon } from '@heroicons/react/solid';
 
-const Story = ({ username, img }) => {
+const Story = ({ username, img, isUser }) => {
   return (
-    <div>
+    <div className="relative group cursor-pointer">
       <img
-        className="h-14 rounded-full p-[1.5px] border-2 border-red-500 cursor-pointer hover:scale-110 transition-transform duration-200 ease-out"
+        className="h-14 rounded-full p-[1.5px] border-2 border-red-500 group-hover:scale-110 transition-transform duration-200 ease-out"
         src={img}
         alt={username}
       />
+      {isUser && <PlusIcon className="h-6 absolute top-4 left-4 text-white" />}
       <p className="text-xs w-14 truncate">{username}</p>
     </div>
   );
