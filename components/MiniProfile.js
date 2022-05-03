788072ae359806fcd60e0bab1b61decccb4d6f34
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-05-03 15:01:44 +0200

    Implement showing google data in mini profile

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -1,20 +1,25 @@
 import React from 'react';
+import { useSession, signOut } from 'next-auth/react';
 
 const MiniProfile = () => {
+  const { data: session } = useSession();
+
   return (
     <div className="flex items-center justify-between mt-14 ml-10">
       <img
         className="h-16 rounded-full border p-[2px]"
-        src="https://www.adscientificindex.com/pictures/0b/50734.jpg"
+        src={session.user.image}
         alt="User image"
       />
 
       <div className="flex-1 ml-4">
-        <h2 className="font-bold">codewithsahand</h2>
+        <h2 className="font-bold">{session.user.username}</h2>
         <h3 className="text-sm text-gray-400">Welcome to Instagram</h3>
       </div>
 
-      <button className="font-semibold text-blue-400 text-sm">Sign out</button>
+      <button onClick={signOut} className="font-semibold text-blue-400 text-sm">
+        Sign out
+      </button>
     </div>
   );
 };
