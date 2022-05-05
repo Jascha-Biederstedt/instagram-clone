331b28a2fabbcf7db8da04cc81c0dcc2e56031e9
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-05-05 15:30:03 +0200

    Add modal UI

--- components/UploadModal.js
+++ components/UploadModal.js
@@ -1,12 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useRecoilState } from 'recoil';
+import Modal from 'react-modal';
+import { CameraIcon } from '@heroicons/react/outline';
 
 import { modalState } from '../atom/modalAtom';
 
 const UploadModal = () => {
   const [open, setOpen] = useRecoilState(modalState);
+  const [selectedFile, setSelectedFile] = useState(null);
 
-  return <div>{open && <h1>OpenModal</h1>}</div>;
+  return (
+    <>
+      {open && (
+        <Modal
+          className="max-w-lg w-[90%] p-6 absolute top-56 left-[50%] translate-x-[-50%] bg-white border-2 rounded-md shadow-md"
+          isOpen={open}
+          onRequestClose={() => {
+            setOpen(false);
+            setSelectedFile(null);
+          }}
+          ariaHideApp={false}
+        >
+          <div className="flex flex-col justify-center items-center h-[100%]">
+            {selectedFile ? (
+              <img
+                onClick={() => setSelectedFile(null)}
+                src={selectedFile}
+                alt=""
+                className="w-full max-h-[250px] object-cover cursor-pointer"
+              />
+            ) : (
+              <CameraIcon className="cursor-pointer h-14 bg-red-200 p-2 rounded-full border-2 text-red-500" />
+            )}
+
+            <input type="file" hidden />
+            <input
+              type="text"
+              maxLength="150"
+              placeholder="Please enter your caption..."
+              className="m-4 border-none text-center w-full focus:ring-0"
+            />
+            <button
+              disabled={!selectedFile || loading}
+              className="w-full bg-red-600 text-white p-2 shadow-md hover:brightness-125 disabled:bg-gray-200 disabled:cursor-not-allowed disabled:hover:brightness-100"
+            >
+              Upload Post
+            </button>
+          </div>
+        </Modal>
+      )}
+    </>
+  );
 };
 
 export default UploadModal;
