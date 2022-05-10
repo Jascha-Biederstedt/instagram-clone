9add3b070cbe19566ed6b66e369d7f92d32a5492
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-05-10 16:13:45 +0200

    Implement showing number of likes

--- components/Post.js
+++ components/Post.js
@@ -109,6 +109,9 @@ const Post = ({ id, username, userImg, img, caption }) => {
       )}
 
       <p className="p-5 truncate">
+        {likes.length > 0 && (
+          <p className="font-bold mb-1">{likes.length} likes</p>
+        )}
         <span className="font-bold mr-2">{username}</span>
         {caption}
       </p>
